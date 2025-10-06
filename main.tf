d3c42995ec9042603700cad475778765d13d82e6
dneelakandan <40385949+dneelakandan@users.noreply.github.com>
2025-10-06 10:59:52 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 
 resource "azurerm_virtual_network" "aks_vnet" {
-  name                = "${var.cluster_name}-vnet"
+  name                = var.vnet_name
   address_space       = ["10.0.0.0/8"]
   location            = var.location
   resource_group_name = var.aks_resource_group
 }
 
 resource "azurerm_subnet" "aks_subnet" {
-  name                 = "${var.cluster_name}-subnet"
+  name                 = "${vnet_name}-subnet"
   resource_group_name  = var.aks_resource_group
   virtual_network_name = azurerm_virtual_network.aks_vnet.name
   address_prefixes     = ["10.240.0.0/16"]
--- variables.tf
+++ variables.tf
@@ -1,12 +1,20 @@
 
-variable "cluster_name" {
-  type = string
+variable "vnet_name" {
+   type        = string
+  description = "Name of the AKS cluster"
+  default     = "lbs-aks-vnet"
 }
 
+
+
 variable "location" {
-  type = string
+  type        = string
+  description = "Azure region for AKS deployment"
+  default     = "West Europe"
 }
 
 variable "aks_resource_group" {
-  type = string
+   type        = string
+  description = "Name of the resource group"
+  default     = "lbs-rg"
 }
